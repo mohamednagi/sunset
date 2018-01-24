bff19e6d66a152eba5c3b876dda63f0ba68ccd62
Sierra <sierra@192.168.134.128>
2018-01-24 00:28:16 -0800

    Initial Commit

---
+++ SunSetApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SunSetApp
+//
+//  Created by Sierra on 1/24/18.
+//  Copyright © 2018 Nagiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
