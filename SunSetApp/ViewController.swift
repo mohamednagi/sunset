c0bb8e659a31dad3a86086c5fdf6e5e9465ca269
mohamed nagi <mohamed_nagi_93@hotmail.com>
2018-03-12 08:48:18 -0700

    added files

--- SunSetApp/ViewController.swift
+++ SunSetApp/ViewController.swift
@@ -1,25 +1,30 @@
-//
-//  ViewController.swift
-//  SunSetApp
-//
-//  Created by Sierra on 1/24/18.
-//  Copyright © 2018 Nagiz. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var laCity: UITextField!
+    @IBOutlet weak var laResult: UILabel!
+    @IBAction func GetYourTime(_ sender: Any) {
+        let url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22\(laCity.text!)%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
+        GetSunSet(url: url)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+ 
+    func GetSunSet (url:String){
+        DispatchQueue.global().async {
+        do {
+            let appurl = URL(string:url)!
+            let data = try Data(contentsOf : appurl)
+            let json = try JSONSerialization.jsonObject(with: data) as! [String:Any]
+            let query = json["query"] as! [String:Any]
+            let results = query["results"] as! [String:Any]
+            let channel = results["channel"] as! [String:Any]
+            let astronomy = channel["astronomy"] as! [String:Any]
+            DispatchQueue.global().sync {
+            self.laResult.text! = "SunSet in \(self.laCity.text!) is : \(astronomy["sunset"]!)"
+            }
+        }catch {
+            print("cannot provide service")
+            }
+            }
 }
-
+}
